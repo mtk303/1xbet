76b5f62868f404810fe41d7bd55b15079ac06388
MayThinKyi303 <maythinkyi2003@gmail.com>
2024-03-02 11:03:35 +0630

    set up

---
+++ src/router/index.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../pages/Layout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+   
+    {
+      path:'/',
+      element:<Layout/>,
+      children:[
+        {
+          path : "/",
+          element : <Home />
+        },
+    ]
+}]);
+
+export default router;
